e763346d459d3c09b78acd05128b7fcb8a2af1f3
932554 <61924694+932554@users.noreply.github.com>
2021-05-16 17:37:07 -0400

    Create Animate.lua

---
+++ Animate.lua
@@ -0,0 +1,42 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local Player = game:GetService("Players").LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait()
+Player.CharacterAdded:Connect(function(Char)
+    Character = Char
+end)
+
+local AnimationIds = {
+    idle1 = 707742142,
+    idle2 = 707855907,
+    run = 707861613,
+    walk = 754636298,
+    jump = 754637084,
+    fall = 754636589,
+    swim = 754638471,
+    swimIdle = 707742142,
+    climb = 754639239
+}
+
+local function AnimatePlayer(Character)
+    local Humanoid = Character:WaitForChild("Humanoid")
+    local Animate = Character:WaitForChild("Animate")
+
+    if Humanoid.RigType == Enum.HumanoidRigType.R15 then
+        Animate.idle.Animation1.AnimationId = "rbxassetid://" .. AnimationIds.idle1
+        Animate.idle.Animation2.AnimationId = "rbxassetid://" .. AnimationIds.idle2
+        Animate.run.RunAnim.AnimationId = "rbxassetid://" .. AnimationIds.run
+        Animate.walk.WalkAnim.AnimationId = "rbxassetid://" .. AnimationIds.walk
+        Animate.jump.JumpAnim.AnimationId = "rbxassetid://" .. AnimationIds.jump
+        Animate.fall.FallAnim.AnimationId = "rbxassetid://" .. AnimationIds.fall
+        Animate.swim.Swim.AnimationId = "rbxassetid://" .. AnimationIds.swim
+        Animate.swimIdle.SwimIdle.AnimationId = "rbxassetid://" .. AnimationIds.swimIdle
+        Animate.climb.ClimbAnim.AnimationId = "rbxassetid://" .. AnimationIds.climb
+    end
+end
+
+AnimatePlayer(Character)
+
+Player.CharacterAdded:Connect(AnimatePlayer)
